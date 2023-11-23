7e41733179ab57411cc69a53aac983b05262eda2
Vincent_Liussyaputra <liusvinv@gmail.com>
2023-11-23 21:46:43 +0700

    Add more stuff

---
+++ schemas/index.ts
@@ -0,0 +1,10 @@
+import artworks from "./artworks";
+import credits from "./credits";
+import games from "./games";
+import games_status from "./games_status";
+import general from "./general";
+import guestbook from "./guestbook";
+import mods from "./mods";
+import showcase from "./showcase";
+
+export const schemaTypes = [general,games,games_status,artworks,showcase,credits,mods,guestbook]
